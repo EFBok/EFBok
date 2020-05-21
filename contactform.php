fa20e82b76d41db291e376ccf4a6d23f772f2ebe
Elijah Bok <elijahbok21@gmail.com>
2020-05-21 17:48:45 +0200

    Changed PHP

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])){
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$subject = $_POST['email'];
+
+	$mailTo = "elijahbok21@gmail.com"
+	$headers = "From: ".$email;
+	$txt = "You recieved an email from" .$name.".\n\n" .$message;
+
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.php?mailsend");
+}
